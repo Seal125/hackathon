e2e88d08046b8d6dc58800aa592a87dbedd90cff
Stephanie <seal12514@gmail.com>
2018-03-17 20:19:05 +0000

    Added stuff

--- js/search.js
+++ js/search.js
@@ -3,10 +3,16 @@ $("#search").click(function (){
         // to loop whatever is in the exibits
     for (var i = 0; i < data.length; i++) {
         var sts = data[i].search_terms;
-        for (var s = 0; s < sts.length;i++ ){}
-        //use this format to append the
-        var comp = createComponent(data[i]);
-        $("#exhibit-list").append(comp);
-
+        var include = false;
+        for (var s = 0; s < sts.length;s++ ){
+            if(sts[s].indexOf(text)!== -1){
+                include = true; 
+            }
+        }
+        if(include){
+            //use this format to append the
+            var comp = createComponent(data[i]);
+            $("#exhibit-list").append(comp);
+        }
     }
 });
